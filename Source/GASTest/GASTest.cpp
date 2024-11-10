65904cec35eca1862e9b89b8cdf8a0b0c6b8f525
zhishao <838674292@qq.com>
2024-11-10 15:13:54 +0800

    Initial commit

---
+++ Source/GASTest/GASTest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GASTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GASTest, "GASTest" );
